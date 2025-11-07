e1421303670703a057fe78197750b9393dbf1a43
layendo1 <layendo1@gmail.com>
2025-11-07 15:38:01 -0100

    Mise à jour

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js - REMPLACEZ par votre vraie clé API Google AI
+window.process = window.process || {};
+window.process.env = window.process.env || {};
+window.process.env.API_KEY = "AIzaSyC8y0956BVOV7YDFTJtQZJnENMKtX8CQMQ";
